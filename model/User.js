b235c7fec55d77338388d7f3814d9f4e3da56fba
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-02-12 17:41:35 +0100

    changed model

--- model/User.js
+++ model/User.js
@@ -18,7 +18,9 @@ const userSchema = new Schema({
         type: String,
         required: true,
         minlength: 6
-    }
+    },
+
+    blogs: [{type: mongoose.Types.ObjectId, ref: "Blog", required: true }],
 
 
 });
